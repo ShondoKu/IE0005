24f0fc96837a3ea501559832d3f96c01ad6d8807
Sean Ku <seanku10@gmail.com>
2023-01-15 23:52:35 +0800

    lab 2 qn2

---
+++ Lab/Lab 2/qn2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+#a)
+html_data = pd.read_html('https://en.wikipedia.org/wiki/2016_Summer_Olympics_medal_table')
+
+#b)
+#print(len(html_data))
+
+#c)
+#print(html_data[2])
+
+#d)
+#data_table = pd.DataFrame(html_data[2])
+#print(data_table)
+
+#e)
+#data_table = pd.DataFrame(html_data[2])
+#top20 = pd.DataFrame(data_table.head(20))
+#print(top20)
