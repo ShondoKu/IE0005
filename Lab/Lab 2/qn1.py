4575e07dd38b5f878758862a2848a9027e3fd3ec
Sean Ku <seanku10@gmail.com>
2023-01-15 23:10:56 +0800

    complete lab 2 q1

--- Lab/Lab 2/qn1.py
+++ Lab/Lab 2/qn1.py
@@ -1,2 +1,19 @@
 import numpy as np
 import pandas as pd
+
+train_data = pd.read_csv("house-prices-advanced-regression-techniques/train.csv")
+
+#a)
+#print(train_data.head())
+
+#b)
+#print("Data dimensions: "+ str(train_data.shape))
+
+#c)
+#print(train_data.dtypes)
+
+#c)
+#print(train_data.info())
+
+#d)
+#print(train_data.describe())
