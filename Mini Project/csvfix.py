646d0382afd557f56dc52702f69c9aa901fc91cb
Sean Ku <seanku10@gmail.com>
2023-11-07 19:29:10 +0800

    ewrwe

--- Mini Project/csvfix.py
+++
@@ -1,9 +0,0 @@
-import csv
-header_columns = []
-csvfile = open('cardio_train.csv').readlines()
-columns = csvfile[0].split(';')
-columns[-1] = 'cardio'
-header_columns = columns
-print(header_columns)
-
-
